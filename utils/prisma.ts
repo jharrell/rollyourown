673b8d9fc83fca2e91fb767b7cb8e9f233f821a5
Jon Harrell <4829245+jharrell@users.noreply.github.com>
2023-08-21 08:34:32 -0500

    import accelerate the Deno way (tm)

--- utils/prisma.ts
+++ utils/prisma.ts
@@ -1,5 +1,6 @@
 import { PrismaClient } from '$prisma/edge.ts';
+import { withAccelerate } from 'npm:@prisma/extension-accelerate@0.5.0';
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient().$extends(withAccelerate());
 
 export default prisma;
